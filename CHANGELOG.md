a5654afffcd18ab303dff3dc4d9748e1e2f98e0e
Jonathan Hartman <j@p4nt5.com>
2014-12-14 23:30:40 -0800

    Initial commit, freshly generated

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Dropbox Cookbook CHANGELOG
+==========================
+
+v0.0.1 (2014-12-14)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'dropbox'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures dropbox'
+long_description 'Installs/Configures dropbox'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
